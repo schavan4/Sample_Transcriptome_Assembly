b78a20416217d1408c2acc69b7912f1886f4af8d
schavan4 <114094937+schavan4@users.noreply.github.com>
2022-12-13 00:33:24 -0800

    Get ALL RNA seq sample runs

--- scripts/getRNAseq.sh
+++ scripts/getRNAseq.sh
@@ -4,4 +4,7 @@
 dirPath=$1
 
 # Retrieve the Thermus thermophilus RNA-seq reads.
-fasterq-dump --split-3 SRR12549440 -O "${dirPath}thermo_RNAseq" -f	#Arguments specify the split mode, RNA-seq read identifier, and output directory
+for i in {40-52}
+do
+  fasterq-dump --split-3 "SRR125494${i}" -O "${dirPath}thermo_RNAseq" -f	#Arguments specify the split mode, RNA-seq read identifier, and output directory
+done
