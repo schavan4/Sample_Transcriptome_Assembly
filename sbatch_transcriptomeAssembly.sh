687de6c2557735638b61f7937a8919528448438e
schavan4 <114094937+schavan4@users.noreply.github.com>
2022-12-12 23:33:06 -0800

    Added trinity transcriptome assembly calls

--- sbatch_transcriptomeAssembly.sh
+++ sbatch_transcriptomeAssembly.sh
@@ -11,6 +11,16 @@ echo "Starting analysis"
 echo "Loading Anaconda env"
 module load anaconda3/2021.11		#Loads the anaconda modules for python
 source activate BINF-12-2021		#Loads the dependencies from /BINF-12-2021
+module load samtools/1.10
 
 echo "Downloading RNAseq reads"
 bash scripts/getRNAseq.sh data/				#Retrieves raw sequence reads
+
+echo "Starting De Novo Assembly $(date)"
+echo "Assemble the De Novo Transcriptome $(date)"
+bash trinityDeNovo.sh data/ 1>results/logs/$SLURM_JOB_NAME-$SLURM_JOB_ID-trinityDeNovo.log 2>../results/logs/$SLURM_JOB_NAME-$SLURM_JOB_ID-trinityDeNovo.err
+
+echo "Analyze the De Novo Transcriptome $(date)"
+bash analyzeTrinityDeNovo.sh data/ 1>results/$SLURM_JOB_NAME-$SLURM_JOB_ID-trinity_de_novo_stats.txt 2>../results/logs/$SLURM_JOB_NAME-$SLURM_JOB_ID-analyzeTrinityDeNovo.err
+
+echo "De Novo Assembly complete $(date)"
