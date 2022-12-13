4b74ca6809d8f86c6dbebd4e5ee91a72d8618363
schavan4 <114094937+schavan4@users.noreply.github.com>
2022-12-12 23:39:25 -0800

    Update sbatch_transcriptomeAssembly.sh

--- sbatch_transcriptomeAssembly.sh
+++ sbatch_transcriptomeAssembly.sh
@@ -18,9 +18,9 @@ bash scripts/getRNAseq.sh data/				#Retrieves raw sequence reads
 
 echo "Starting De Novo Assembly $(date)"
 echo "Assemble the De Novo Transcriptome $(date)"
-bash trinityDeNovo.sh data/ 1>results/logs/$SLURM_JOB_NAME-$SLURM_JOB_ID-trinityDeNovo.log 2>../results/logs/$SLURM_JOB_NAME-$SLURM_JOB_ID-trinityDeNovo.err
+bash trinityDeNovo.sh data/ 1>results/logs/$SLURM_JOB_NAME-$SLURM_JOB_ID-trinityDeNovo.log 2>results/logs/$SLURM_JOB_NAME-$SLURM_JOB_ID-trinityDeNovo.err
 
 echo "Analyze the De Novo Transcriptome $(date)"
-bash analyzeTrinityDeNovo.sh data/ 1>results/$SLURM_JOB_NAME-$SLURM_JOB_ID-trinity_de_novo_stats.txt 2>../results/logs/$SLURM_JOB_NAME-$SLURM_JOB_ID-analyzeTrinityDeNovo.err
+bash analyzeTrinityDeNovo.sh data/ 1>results/$SLURM_JOB_NAME-$SLURM_JOB_ID-trinity_de_novo_stats.txt 2>results/logs/$SLURM_JOB_NAME-$SLURM_JOB_ID-analyzeTrinityDeNovo.err
 
 echo "De Novo Assembly complete $(date)"
