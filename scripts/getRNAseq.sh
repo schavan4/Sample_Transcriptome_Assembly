51f758f709bca18072c85c3dd36d596051a1af53
schavan4 <114094937+schavan4@users.noreply.github.com>
2022-12-13 22:41:30 -0800

    Update getRNAseq.sh

--- scripts/getRNAseq.sh
+++ scripts/getRNAseq.sh
@@ -5,12 +5,8 @@ dirPath=$1
 
 mkdir -p "${dirPath}thermo_RNAseq"
 
-# ONLY Retrieving the WT control group RNAseq run for this assignment to save on compute time, as I did not find small-size QC runs were found on SRA Run Selector
-
-fasterq-dump --split-3 "SRR12549443" -O "${dirPath}thermo_RNAseq" -f	#Arguments specify the split mode, RNA-seq read identifier, and output directory
-
 # To get all RNA seq runs of all treatment groups in the experiment
-#for i in {40..52}
-#do
-#    fasterq-dump --split-3 "SRR125494${i}" -O "${dirPath}thermo_RNAseq" -f	#Arguments specify the split mode, RNA-seq read identifier, and output directory
-#done
+for i in {40..52}
+do
+    fasterq-dump --split-3 "SRR125494${i}" -O "${dirPath}thermo_RNAseq" -f	#Arguments specify the split mode, RNA-seq read identifier, and output directory
+done
