1431863258a4947615a2223f7b9eec66cc87e57c
schavan4 <114094937+schavan4@users.noreply.github.com>
2022-12-14 09:55:43 -0800

    Update prepareForIPS.sh

--- scripts/prepareForIPS.sh
+++ scripts/prepareForIPS.sh
@@ -2,4 +2,6 @@
 # prepareForIPS.sh
 # Usage: bash prepareForIPS.sh <lines> <data> <output> 1>results/logs/prepareForIPS.log 2>results/logs/prepareForIPS.err
 
-head -$1 $2 | sed 's/*//' > $3
+lines=$1
+
+head "-${lines}" $2 | sed 's/*//' > $3
