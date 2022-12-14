9d95e77564e33f012f51aa043b1e720fb25430a8
schavan4 <114094937+schavan4@users.noreply.github.com>
2022-12-14 01:42:10 -0800

    Update runIPS.sh

--- scripts/runIPS.sh
+++ scripts/runIPS.sh
@@ -2,4 +2,4 @@
 # runIPS.sh
 # Usage: bash scripts/runIPS.sh <data> <output> 1>results/logs/runIPS.log 2>results/logs/runIPS.err
 
-interproscan.sh -cpu 2 -dp -f tsv -goterms -i $1 -o $2 -pa
+interproscan.sh -cpu 32 -dp -f tsv -goterms -i $1 -o $2 -pa
