2a6edfe4660455c499d2da8dd0bda515a332aacf
schavan4 <114094937+schavan4@users.noreply.github.com>
2022-12-14 00:09:44 -0800

    Create pfamScan_args.sh

---
+++ scripts/pfamScan_args.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# pfamScan.sh
+# Usage: bash scripts/pfamScan.sh <dotmblout> <longestOrfs> 1>results/logs/pfamScan.log 2>results/logs/pfamScan.err
+
+#<dotmblout> /results/pfam.domtblout
+#<longestOrfs> /results/trinity_de_novo.transdecoder_dir/longest_orfs.pep
+
+hmmscan --cpu 4 --domtblout $1 $2 $3
