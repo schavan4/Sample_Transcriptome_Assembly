9533fdea3d27a84dda2042d8c44e3533aad783d0
schavan4 <114094937+schavan4@users.noreply.github.com>
2022-12-14 19:29:25 -0800

    Update getNGS.sh

--- scripts/getNGS.sh
+++ scripts/getNGS.sh
@@ -4,4 +4,4 @@
 dirPath=$1
 
 # Retrieve the Rhodobacter spheroides NGS reads.
-fasterq-dump --split-3 SRR12549453 -O "${dirPath}thermo" -f	#Arguments specify the split mode, NGS identifier, and output directory
+fasterq-dump --split-3 SRR12549453 -O "${dirPath}DNAseq" -f	#Arguments specify the split mode, NGS identifier, and output directory
