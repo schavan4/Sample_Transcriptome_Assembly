c4fb2385db13d24d5952c848d512b0cac6e1293e
schavan4 <114094937+schavan4@users.noreply.github.com>
2022-12-14 20:00:39 -0800

    Update getRNAseq.sh

--- scripts/getRNAseq.sh
+++ scripts/getRNAseq.sh
@@ -9,7 +9,7 @@ srrMax=$4
 mkdir -p "${dirPath}RNAseq"
 
 # To get all RNA seq runs of all treatment groups in the experiment
-for i in {$srrMin..$srrMax}
+for i in $(seq $srrMin $srrMax)
 do
     fasterq-dump --split-3 "${srrNum}${i}" -O "${dirPath}RNAseq" -f	#Arguments specify the split mode, RNA-seq read identifier, and output directory
 done
