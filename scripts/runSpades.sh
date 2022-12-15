ce537b51d34d30d191f0e5645b08fff839838d5e
schavan4 <114094937+schavan4@users.noreply.github.com>
2022-12-14 19:31:20 -0800

    Update runSpades.sh

--- scripts/runSpades.sh
+++ scripts/runSpades.sh
@@ -4,13 +4,13 @@
 # following function takes in the left and right reads, then outputs the data in /rhodo/
 dirPath=$1
 
-mkdir -p "${dirPath}thermo/spades"
+mkdir -p "${dirPath}DNAseq/spades"
 
 function Spades {
     spades.py \
-    -1 "${dirPath}trimmed/paired/Thermo.R1.paired.fastq" \
-    -2 "${dirPath}trimmed/paired/Thermo.R2.paired.fastq" \
-    -o "${dirPath}thermo/spades"
+    -1 "${dirPath}trimmed/paired/DNAseq.R1.paired.fastq" \
+    -2 "${dirPath}trimmed/paired/DNAseq.R2.paired.fastq" \
+    -o "${dirPath}DNAseq/spades"
 }
 
 Spades # runs the function Spades
